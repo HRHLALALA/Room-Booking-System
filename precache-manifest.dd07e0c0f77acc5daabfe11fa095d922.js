921d224183bdf99a7f620d863b63c1dbbe297c25
Renhao Huang <z5111342@ad.unsw.edu.au>
2019-09-11 10:46:53 +0800

    Updates

---
+++ precache-manifest.dd07e0c0f77acc5daabfe11fa095d922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9420657931f6ac5780c3ea8f6655f379",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5d686cca70d501ab27a0",
+    "url": "/my-app/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "1b3a63d0277a4d32f2c4",
+    "url": "/my-app/static/css/main.3bcbb460.chunk.css"
+  },
+  {
+    "revision": "5d686cca70d501ab27a0",
+    "url": "/my-app/static/js/2.95edb178.chunk.js"
+  },
+  {
+    "revision": "1b3a63d0277a4d32f2c4",
+    "url": "/my-app/static/js/main.d951113d.chunk.js"
+  },
+  {
+    "revision": "f84fc7974046a8134367",
+    "url": "/my-app/static/js/runtime~main.5310139a.js"
+  }
+]);
